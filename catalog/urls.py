adf036dd93ace6e245ac09e2260a4af6bb9dbf8f
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-10-06 23:26:37 +0300

    fint

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,11 +5,7 @@ from catalog.views import home_page, adress_page
 
 
 urlpatterns = [
-<<<<<<< HEAD
+
     path('', home_page),
     path('/catalog1', adress_page),
-=======
-    path('', adress_page),
-    path('', home_page),
->>>>>>> origin/main
 ]
