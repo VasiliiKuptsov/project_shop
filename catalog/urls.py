4cd830efadfbf71c4c5609d9e41e5841af4c80b5
VasiliiKuptsov <kuptsovy@mail.ru>
2023-10-08 15:42:23 +0300

    undestand

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,9 +7,6 @@ from catalog.views import home_page, adress_page
 urlpatterns = [
 
     path('', home_page),
-<<<<<<< HEAD
+    path('catalog1', adress_page),
+
     ]
-=======
-    path('/catalog1', adress_page),
-]
->>>>>>> adf036dd93ace6e245ac09e2260a4af6bb9dbf8f
